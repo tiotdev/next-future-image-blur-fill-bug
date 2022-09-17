d67ee44b0b969565dc1ccfffc60a8b38721f5a8c
tiotdev <tiotdev@tiot.de>
2022-09-17 23:28:38 +0300

    Add example

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import FutureImage from "next/future/image";
+
 export default function Home() {
   return (
-    <p>
-      <div />
-    </p>
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "200px",
+          backgroundColor: "red",
+          position: "relative",
+        }}
+      >
+        <Image
+          src="https://img.travelfeed.io/jpphotography%2F20200609T140211386Z-reisen-127.jpg"
+          alt=""
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAI0lEQVR4nGMINGf4/+3at/t7+2viGNQZGKbE69UlOSV5mgEAnuYKmizl818AAAAASUVORK5CYII="
+          placeholder="blur"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "200px",
+          backgroundColor: "red",
+          position: "relative",
+        }}
+      >
+        <FutureImage
+          src="https://img.travelfeed.io/jpphotography%2F20200609T140211386Z-reisen-127.jpg"
+          alt=""
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAI0lEQVR4nGMINGf4/+3at/t7+2viGNQZGKbE69UlOSV5mgEAnuYKmizl818AAAAASUVORK5CYII="
+          placeholder="blur"
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </>
   );
 }
